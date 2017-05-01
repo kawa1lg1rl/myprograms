59d72d249ced11981c21e20b15c74268d8c41688
kawa1lg1rl <bdb01113@naver.com>
2017-05-01 23:39:19 +0900

    fb_auto_poker update

--- fb_auto_poker/poke.py
+++ fb_auto_poker/poke.py
@@ -2,7 +2,9 @@
 #-*- coding: utf-8 -*-
 import cookielib, urllib, urllib2, httplib, time
 #import ssl
-import sys
+import sys, re
+
+# 한글
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -23,8 +25,8 @@ class poke():
                 
                 while 1:
                     self.poke()
-            except:
-                print "Error!"
+            except Exception as e:
+                print e
                 print ""
                 
         
@@ -36,20 +38,33 @@ class poke():
         req = urllib2.Request("https://m.facebook.com/login.php?refsrc=https%3A%2F%2Fm.facebook.com%2F&refid=8", data)
         op = urllib2.urlopen(req)
 
+        print "Login!"
+
     def poke(self):
         self.name_list = []
         req = urllib2.Request(self.url_poke)
         op = urllib2.urlopen(req)
         rd = op.read()
 
+        poke_count = []
         poke_list = rd.split("나도 콕 찔러보기")
+        
         del(poke_list[-1])
 
         if len(poke_list) == 0:
             time.sleep(1)
             return 0
 
+        p = re.compile("[0-9]")
+        for i in range(0,len(poke_list)):
+            
+            temp = unicode(poke_list[i][poke_list[i].rfind("회원님을"):poke_list[i].find("번 ")])
+            temp = p.findall(temp)
+            temp = "".join(temp)
+            poke_count.append(temp)
+            
         # find user name
+        p = re.compile("")
         for i in poke_list:
             temp = i.split("님이 회원님을")[0]
             cnt = 0
@@ -77,9 +92,9 @@ class poke():
         for i in range(0, len(self.name_list)):
             req = urllib2.Request(self.url_main+self.url_list[i])
             op = urllib2.urlopen(req)
-            print(unicode(self.name_list[i].replace("</s","") + " 님을 찔렀습니다"))
+            print(unicode(self.name_list[i].replace("</s","") + "님(%s)을 찔렀습니다."%poke_count[i]))
             time.sleep(0.001)
-        time.sleep(2)
+        time.sleep(0.5)
         
         
 
